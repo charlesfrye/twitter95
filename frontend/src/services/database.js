5f1d943a63f2a4742ca80dd8c6b1cd7dc3fc5e2a
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2024-04-14 01:07:01 -0700

    reverts changes to db service, brings in new url

--- frontend/src/services/database.js
+++ frontend/src/services/database.js
@@ -1,8 +1,8 @@
-// TODO: actually fetch posts data from the database:
-
 const fetchData = async () => {
   try {
-    const response = await fetch("/database.json");
+    const response = await fetch(
+      "https://ex-twitter--db-client-api.modal.run/tweets"
+    );
     const data = await response.json();
     return data;
   } catch (error) {
@@ -15,18 +15,18 @@ const getFeed = async (limit, realTime, userId) => {
   // fetch limit tweets as of realTime (optionally for userId) from the db
   const response = await fetchData();
 
-  return response.feed;
+  return response;
 };
 
 const getTimeline = async (limit, realTime, userId) => {
   // fetch limit most recent tweets as of realTime (optionally for userId) from the db
-  const response = await fetchData().tweets;
+  const response = await fetchData();
   return response;
 };
 
 const getPosts = async (limit, realTime, userId) => {
   // fetch limit most recent posts as of realTime for a given userId from the db (to put in profile page)
-  const response = await fetchData().tweets;
+  const response = await fetchData();
   return response;
 };
 
