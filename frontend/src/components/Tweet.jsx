33307a12ad8fc808b606e39c977fd184677c06b6
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2024-04-13 18:25:40 -0700

    Updates tweet proptypes to match database

--- frontend/src/components/Tweet.jsx
+++ frontend/src/components/Tweet.jsx
@@ -10,8 +10,19 @@ function Tweet({ tweet }) {
 
 Tweet.propTypes = {
   tweet: PropTypes.shape({
-    id: PropTypes.number,
+    id: PropTypes.string,
     text: PropTypes.string,
+    media: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.arrayOf(PropTypes.string),
+    ]),
+    author: PropTypes.string, // User:id or User object?
+    root: PropTypes.string, // Tweet:id or Tweet object?
+    // quotedBy: PropTypes.string, // User:id or User object?
+    // retweetedBy: PropTypes.string, // User:id or User object?
+    quoted: PropTypes.string, // Tweet:id or Tweet object?
+    retweeted: PropTypes.string, // Tweet:id or Tweet object?
+    likedBy: PropTypes.arrayOf(PropTypes.string),
     timestamp: PropTypes.string,
     userId: PropTypes.string,
   }),
