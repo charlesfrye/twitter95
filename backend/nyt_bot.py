fec24762fa76128685e12df7f88faa5076558e2d
Charles Frye <charles@modal.com>
2024-04-14 08:29:07 -0700

    updates to nyt_bot.py

--- backend/nyt_bot.py
+++ backend/nyt_bot.py
@@ -1,90 +1,125 @@
-import modal
-import os
-import pandas as pd
 from datetime import datetime, timezone
-from pydantic import BaseModel
-from typing import Optional
-import math
 
-stub = modal.Stub("nyt_bot", secrets=[modal.Secret.from_name("nyt-api-key")])
+import modal
 
-class CreateTweetRequest(BaseModel):
-    author: str
-    text: str
-    images: Optional[list[str]] = []
-    faketime: Optional[datetime] = None
-    
-class CreateUserRequest(BaseModel):
-    profilePic: str
-    bannerPic: str
-    username: str
-    displayName: str
-    
+from .common import image
+
+nyt_image = image.pip_install("pandas", "requests")
+volume = modal.Volume.from_name("nyt-bot-volume", create_if_missing=True)
+
+stub = modal.Stub(
+    "nyt_bot",
+    # secrets=[modal.Secret.from_name("nyt-api-key")],
+    image=nyt_image,
+    volumes={"/archive": volume},
+)
+
+with nyt_image.imports():
+    import pandas as pd
+    import requests
+
+    from . import models
+
+
+@stub.function(_allow_background_volume_commits=True)
+def go():
+    user = create_or_fetch_user()
+    df = load_articles()
+    add_tweets(df, user)
+
+
+def add_tweets(df, user):
+    for index, row in df.iterrows():
+        tweet = models.TweetCreate(
+            author=user,
+            text=row["headline"],
+            fake_time=generate_faketime_utc_timestamp(
+                row["pub_date"], calculate_publish_hour(index, len(df))
+            ),
+        )
+        try:
+            response = requests.post(
+                "https://ex-twitter--db-client-api.modal.run/tweets/", json=tweet.dict()
+            )
+            response.raise_for_status()
+        except Exception as e:
+            print(f"error posting tweet {index}")
+            print(e)
+
+
+def load_articles(day=None):
+    file = "/archive/new_york_times_stories_1995_april.csv"
+    df = pd.read_csv(file)
+    df = df[["headline", "word_count", "url", "pub_date"]]
+    if day is None:
+        day = datetime.utcnow().day
+    articles = df[df["pub_date"] == f"1995-04-{day.zfill(2)}T05:00:00+0000"]
+
+    return articles
+
+
 def generate_faketime_utc_timestamp(date, hour) -> datetime:
     """returns the datetime object for the given date and hour in UTC"""
     naive_datetime = datetime.strptime(f"{date} {hour}:00:00", "%Y-%m-%d %H:%M:%S")
-    # Attach UTC timezone information
     utc_datetime = naive_datetime.replace(tzinfo=timezone.utc)
     return utc_datetime
+
 
 def calculate_publish_hour(article_index, total_articles, start_index=0):
     if total_articles == 0:
         raise ValueError("Total articles must be greater than 0")
-    
+
     normalized_index = article_index - start_index
-    # 24 hours in a day
     hours_in_day = 24
 
-    # Calculate the number of articles per hour
     articles_per_hour = total_articles / hours_in_day
 
-    # Calculate which hour to publish the article in
     publish_hour = int(normalized_index / articles_per_hour)
 
-    # Ensure that the hour doesn't exceed 23 due to integer division peculiarities
     publish_hour = min(publish_hour, hours_in_day - 1)
-    
+
     return publish_hour
+
+
+def create_or_fetch_user():
+    userName = "NewYorkTimes"
+    try:
+        response = requests.get(
+            f"https://ex-twitter--db-client-api.modal.run/names/{userName}"
+        )
+        response.raise_for_status()
+        data = response.json()
+        if data is None:
+            print(f"User {userName} not found. Creating...")
+            return create_user()
+        else:
+            user = models.UserRead(**data)
+            return user
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        raise
+
+
+def create_user():
+    create_req = models.UserCreate(
+        profile_pic="https://1000logos.net/wp-content/uploads/2017/04/New-York-Times-emblem.jpg",
+        banner_pic="https://1000logos.net/wp-content/uploads/2017/04/Font-New-York-Times-Logo.jpg",
+        user_name="NewYorkTimes",
+        display_name="New York Times Newspaper",
+    )
+
+    try:
+        response = requests.post(
+            "https://ex-twitter--db-client-api.modal.run/users/", json=create_req.dict()
+        )
+        response.raise_for_status()
+        user = models.UserRead(**response.json())
+        return user
+    except Exception as e:
+        print(e)
+        raise
+
 
 @stub.local_entrypoint()
 def main():
-    # Create NYT account
-    create_req = CreateUserRequest(
-        profilePic="https://1000logos.net/wp-content/uploads/2017/04/New-York-Times-emblem.jpg",
-        bannerPic="https://1000logos.net/wp-content/uploads/2017/04/Font-New-York-Times-Logo.jpg",
-        username="NewYorkTimes",
-        displayName="New York Times Newspaper"
-    )
-    # call the create endpoint TBD
-    
-    # load CSV file from ../archive/new_york_times_stories_1995_april.csv of headlines and links
-    file = "../archive/new_york_times_stories_1995_april.csv"
-    df = pd.read_csv(file)
-    print(df.head())
-    # we only want headline, word_count, url, and pub_date
-    df = df[["headline", "word_count", "url", "pub_date"]]
-    print(df.head())
-    
-    for i in range(1, 30):
-        start_index = 0
-        # make sure i has 2 digits
-        if i < 10:
-            i = f"0{i}"
-        articles_for_date = df[df["pub_date"] == f"1995-04-{i}T05:00:00+0000"]
-        print(len(articles_for_date))
-        # iterate through articles with index:
-        for index, article in articles_for_date.iterrows():
-            if start_index == 0:
-                start_index = index
-            tweet_req = CreateTweetRequest(
-                author="nyt_bot", # todo: find ID
-                text=article["headline"],
-                faketime=generate_faketime_utc_timestamp(f"1995-04-{i}", calculate_publish_hour(index, len(articles_for_date), start_index)),
-                images=[article["url"]]
-            )
-            print(tweet_req)
-            
-    # get the set of csv files for the day
-    # divide by 24 hours
-    # tweet out the headlines for the hour
-    print("Hello, World!")
+    go.remote()
