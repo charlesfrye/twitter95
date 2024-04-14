d5f61e155297ac855b193c011d9fc50c8dbe54dc
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2024-04-14 02:26:37 -0700

    Adds windows 95 react library to app, sidebar, and tweets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,32 @@ import Feed from "./components/Feed";
 import Profile from "./components/Profile";
 import Sidebar from "./components/Sidebar";
 import { FaHome, FaBell, FaUserAlt, FaCog, FaSearch } from "react-icons/fa";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { styleReset } from "react95";
+import original from "react95/dist/themes/original";
+
+// React95 fonts
+import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
+import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
+
+const GlobalStyles = createGlobalStyle`
+  ${styleReset}
+  @font-face {
+    font-family: 'ms_sans_serif';
+    src: url('${ms_sans_serif}') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+  }
+  @font-face {
+    font-family: 'ms_sans_serif_bold';
+    src: url('${ms_sans_serif_bold}') format('woff2');
+    font-weight: bold;
+    font-style: normal;
+  }
+  body, input, select, textarea {
+    font-family: 'ms_sans_serif';
+  }
+`;
 
 function App() {
   const { theme } = useTheme();
@@ -26,10 +52,13 @@ function App() {
   return (
     <div className="app">
       <Sidebar className="sidebarLeft" options={leftSidebarOptions} />
-      <Routes>
-        <Route path="/" element={<Feed />} />
-        <Route path="/profile/:userId" element={<Profile />} />
-      </Routes>
+      <ThemeProvider theme={original}>
+        <GlobalStyles />
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </ThemeProvider>
       <Sidebar className="sidebarRight" options={rightSidebarOptions} />
     </div>
   );
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,13 +2,21 @@
 import SidebarOption from "./SidebarOption"; // Make sure the path is correct
 
 function Sidebar({ options, className }) {
+  function handleClick() {
+    alert("Congrats! You just Tweeted!");
+  }
+
   return (
     <div className={`sidebar ${className}`}>
-      <img src="../../public/logo.png" alt="Twitter Logo" />
+      <img
+        src="../../public/logo.png"
+        alt="Twitter Logo"
+        className="logo-animate"
+      />
       {options.map((option, index) => (
         <SidebarOption key={index} Icon={option.icon} text={option.text} />
       ))}
-      <button>Click me to Tweet!</button>
+      <button onClick={handleClick}>Click me to Tweet!</button>
     </div>
   );
 }
