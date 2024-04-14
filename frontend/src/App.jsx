a5a4aae41a681dd161221985187e78ae11e6ca5c
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2024-04-13 21:35:38 -0700

    Builds out App.jsx in case it should be used instead of Feed as home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,33 @@
 import "./App.css";
+import { useEffect } from "react";
+import { useTheme } from "./components/ThemeContext";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Feed from "./components/Feed";
+// import Profile from "./components/Profile";
 
 function App() {
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    console.log(`Updating body class to ${theme}`);
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div>
-      <h1>Hello from the main page of the app!</h1>
-      <p>Here are some examples of links to other pages</p>
-      <nav>
-        <ul>
-          <li>
-            <a href="profile">Profile page</a>
-          </li>
-        </ul>
-      </nav>
+    <div className="app">
+      <header className="nav">
+        <Link to="/feed">Feed </Link>
+        <Link to="/profile">Profile</Link>
+      </header>
+      <div className="main-content">
+        <div className="sidebar left-sidebar"></div>
+        <div className="content">
+          <Router>
+            <Route path="/feed" component={Feed} />
+          </Router>
+        </div>
+        <div className="sidebar right-sidebar"></div>
+      </div>
     </div>
   );
 }
