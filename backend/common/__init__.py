6a538006aee839529dfa3d7ca0840af6d0a3a2de
Charles Frye <charles@modal.com>
2024-05-18 21:18:42 -0400

    add to_real, refactor

--- backend/common/__init__.py
+++ backend/common/__init__.py
@@ -5,4 +5,4 @@ from .utils import to_fake
 mount = modal.Mount.from_local_python_packages("common")
 
 
-__all__ = ["to_fake", "mount"]
+__all__ = ["to_fake", "to_real", "mount"]
--- backend/common/utils.py
+++ backend/common/utils.py
@@ -1,8 +1,14 @@
 from datetime import datetime, timedelta
 
+delta = timedelta(seconds=915_235_088)  # rough number of seconds from 1995 to 2024
+
 
 def to_fake(real_time: datetime) -> datetime:
-    delta = timedelta(seconds=915_235_088)  # rough number of seconds from 1995 to 2024
     fake_time = real_time - delta
     fake_time = fake_time.replace(tzinfo=None)
     return fake_time
+
+
+def to_real(fake_time: datetime) -> datetime:
+    real_time = fake_time + delta
+    return real_time
