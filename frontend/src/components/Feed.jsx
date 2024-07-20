1a8f7879451e1f56a7a71d65b19b572eed57128e
TeddyFrye <111472179+TeddyFrye@users.noreply.github.com>
2024-07-20 12:19:23 -0500

    Removes useTheme from Feed.jsx

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { getTimeline } from "../services/database";
 import { useLocation } from "react-router-dom";
 import Tweet from "./Tweet";
-import { useTheme } from "./ThemeContext";
 import TweetCount from "./TweetCount";
 import Loading from "./Loading";
 
@@ -10,7 +9,6 @@ function Feed() {
   const location = useLocation();
   const [tweets, setTweets] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const { theme } = useTheme();
 
   const queryParams = new URLSearchParams(location.search);
   const fakeTime = queryParams.get("fakeTime");
@@ -28,9 +26,6 @@ function Feed() {
     fetchFeed();
   }, [userId, fakeTime]);
 
-  useEffect(() => {
-    document.body.className = theme;
-  }, [theme]);
   return (
     <div className="Feed">
       {isLoading && <Loading />}
