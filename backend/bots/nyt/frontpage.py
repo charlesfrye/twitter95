783f3c744c6075db4362e2fe193c7378acb89f2f
Charles Frye <charles@modal.com>
2024-08-07 13:04:28 -0400

    cleans up NYT frontpage bot

--- backend/bots/nyt/frontpage.py
+++ backend/bots/nyt/frontpage.py
@@ -6,22 +6,22 @@ import modal
 import common
 import bots.common
 from . import common as nyt_common
-from .common import app
 
 
 BOT_USER_NAME = "NewYorkTimes"
 BOT_DISPLAY_NAME = "New York Times Newspaper"
 
+app = modal.App("nyt-frontpage", image=modal.Image.debian_slim(python_version="3.11"))
+app.include(nyt_common.app)
 
-@app.function(schedule=modal.Period(hours=1))
+
+@app.function(
+    schedule=modal.Period(hours=1), volumes=nyt_common.volumes, image=nyt_common.image
+)
 async def post_nyt_articles(
     fake_time: datetime = None, lookahead_hours: int = None, dryrun: bool = False
 ):
-    bot_id = nyt_common.get_or_create_bot_id.local(
-        user_name=BOT_USER_NAME,
-        display_name=BOT_DISPLAY_NAME,
-        profile_pic=nyt_common.BOT_PROFILE_PIC,
-    )
+    bot_id = nyt_common.get_bot_id.local(user_name=BOT_USER_NAME)
 
     if fake_time is None:
         fake_time = common.to_fake(datetime.utcnow())
@@ -32,13 +32,13 @@ async def post_nyt_articles(
     lookahead = timedelta(hours=lookahead_hours)
 
     while fake_time < (start_hour + lookahead):
-        articles = nyt_common.get_articles_at_hour(fake_time)
-        if articles:
+        articles = nyt_common.get_articles_at_hour(fake_time, sort=True)
+        if len(articles):
             print(f"posting articles for {fake_time:%Y-%m-%d %H}")
             posted = 0
             for article in articles:
                 try:
-                    if article := filter_article(article):
+                    if accept_article(article):
                         if text := nyt_common.parse_article(article):
                             if dryrun:
                                 print("would have posted", text)
@@ -61,20 +61,22 @@ async def post_nyt_articles(
         fake_time += timedelta(hours=1)
 
 
-def filter_article(article):
+def accept_article(article):
     try:
         if (
-            article["type_of_material"] in ["News", "Op-Ed"]
+            article["type_of_material"] in ["News", "Op-Ed"]  # news
+            and article["print_section"] == "A"  # front section
             and len(article["lead_paragraph"].strip()) >= 20
+            and "was married" not in article["lead_paragraph"]
         ):
-            if "was married" not in article["lead_paragraph"]:
-                return article
+            if article["headline"]["main"].strip().upper() != "NO HEADLINE":
+                return True
         else:
-            print("article not of type News or Op-Ed or lead paragraph too short")
+            print("article skipped")
     except Exception as e:
         print(e)
 
 
 @app.local_entrypoint()
-def main(fake_time: str = None, lookahead_hours: int = None):
-    nyt_common.get_or_create_bot_id.remote(user_name=BOT_USER_NAME)
+def main(fake_time: str = None, lookahead_hours: int = None, dryrun: bool = True):
+    post_nyt_articles.remote(fake_time, lookahead_hours, dryrun=dryrun)
