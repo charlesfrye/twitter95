179c2c624eb95dd87af5089bea47de09e2cefbbf
Charles Frye <charles@modal.com>
2025-09-29 17:23:20 -0700

    remove common.mount

--- backend/common/__init__.py
+++ backend/common/__init__.py
@@ -2,7 +2,5 @@ import modal
 
 from .utils import to_fake, to_real, screenshot_tweet
 
-mount = modal.Mount.from_local_python_packages("common")
-
 
 __all__ = ["to_fake", "to_real", "mount", "screenshot_tweet"]
